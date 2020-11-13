24a81538277dd6ffb482d6088dd2d704e82784bd
Hailo <jose.mallma@pucp.pe>
2020-11-12 19:02:16 -0500

    :sparkles: Accept pull requests

--- api/management/management.router.js
+++ api/management/management.router.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 router.use("/projects", require("./project/project.router"));
-
+router.use("/clients", require("./client/client.router"));
 
 module.exports = router;
